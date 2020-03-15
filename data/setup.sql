e5f0aa7b351bcd17f9f1e5ca080643cdaedc808a
Muhamad Risman <real.risman@gmail.com>
2020-03-15 17:30:21 +0700

    Initial dabase setup

---
+++ data/setup.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    uuid VARCHAR(64) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE sessions
+(
+    id serial PRIMARY KEY,
+    uuid VARCHAR(64) NOT NULL UNIQUE,
+    email VARCHAR(255),
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE threads
+(
+    id serial PRIMARY KEY,
+    uuid varchar(64) NOT NULL UNIQUE,
+    topic text,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE posts
+(
+    id serial PRIMARY KEY,
+    uuid VARCHAR(64) NOT NULL UNIQUE,
+    body TEXT,
+    user_id INTEGER REFERENCES users(id),
+    thread_id INTEGER REFERENCES threads(id),
+    created_at TIMESTAMP NOT NULL
+);
