b841e8cfb7341ed4b44185f65015937c37aed8b3
Muhamad Risman <real.risman@gmail.com>
2020-03-15 17:31:07 +0700

    Add connector to the chichat db

---
+++ data/data.go
@@ -0,0 +1,18 @@
+package data
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Db - initiate DB
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "dbname=chitchat sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
